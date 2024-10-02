b66790b8559b1ee735fc672187ac851f6ff52c80
S.Nallaperuma <it21306822@my.sliit.lk>
2024-10-02 10:05:02 +0530

    main.rs update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use bevy::prelude::*;
 use system::update_timer_display;
 
-
 mod component;
 mod system;
 mod input;
@@ -9,18 +8,18 @@ mod input;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(component::GameTimer(None, false)) // Add game timer resource
-        .insert_resource(component::ShipLives(5)) // Add resource for 5 ship lives
-        .add_startup_system(system::setup) // Set up the initial entities
-        .add_system(system::box_movement) // Add box movement system
-        .add_system(input::ship_movement) // Add ship movement system
-        .add_system(input::rotate_ship_on_click) // Add mouse click ship rotation system
-        .add_system(input::shoot_laser) // Add laser shooting system
-        .add_system(system::move_laser) // Add laser movement system
-        .add_system(system::detect_laser_collision) // Add laser-box collision system
+        .insert_resource(component::GameTimer(None, false)) // Game timer resource
+        .insert_resource(component::ShipLives(5)) // 5 ship lives
+        .add_startup_system(system::setup) // Setup entities
+        .add_system(system::box_movement) // Box movement
+        .add_system(input::ship_movement) // Ship movement
+        .add_system(input::rotate_ship_on_click) // Ship rotation on mouse click
+        .add_system(input::shoot_laser) // Laser shooting
+        .add_system(system::move_laser) // Laser movement
+        .add_system(system::detect_laser_collision) // Laser and box collision detection
         .add_system(input::check_end_point_reached) // Check if ship reaches end point
-        .add_system(system::update_timer_display) // Display the timer on the screen
-        .add_system(system::animate_fireball) // Add fireball animation system
-        .add_system(input::detect_collision_and_spawn_fireballs) // Add collision detection and fireball system
+        .add_system(system::update_timer_display) // Display timer
+        .add_system(system::animate_fireball) // Animate fireball
+        .add_system(input::detect_collision_and_spawn_fireballs) // Detect collision between ship and boxes
         .run();
 }
